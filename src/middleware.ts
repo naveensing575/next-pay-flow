e55e38df1fdb36f2614ce4d3612f9329e91d1ce7
naveensing575 <naveensing575@gmail.com>
2025-10-01 12:07:56 +0530

    fix: update middleware routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,5 @@
 export { default } from "next-auth/middleware"
 
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/checkout/:path*",
-    "/api/payments/create-order",
-    "/api/payments/verify-payment"
-  ]
+  matcher: ["/dashboard/:path*", "/checkout/:path*", "/api/payments/:path*"]
 }
